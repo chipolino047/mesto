4868819279d0c182171c7387561c742140b311b3
Vladimir Kurenkov <chipolino047@gmail.com>
2023-03-14 13:25:11 +0400

    Сделал открытие и закрытие popup

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-let openPopup = document.querySelector('border-pen')
+let pen = document.querySelector('.border-pen');
+let popup = document.querySelector('.popup');
+let popupClose = document.querySelector('.popup__close-img');
 
 function open() {
-    if {
-        
-    }
+    popup.classList.add('popup_open');
 }
+pen.addEventListener('click', open);
+
+
+function close() {
+    popup.classList.remove('popup_open');
+}
+popupClose.addEventListener('click', close);
+
+
+
+
+let heart = document.querySelectorAll('.element__heart');
+
+function heartClick() {
+    heart.classList.add('element__heart_active');
+}
+heart.addEventListener('click', heartClick);
