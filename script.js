4249ab626643d640be507be6cb1300c7e9d95f5d
Vladimir Kurenkov <chipolino047@gmail.com>
2023-03-15 20:55:34 +0400

    Feat: реализовал открытие и закрытие попапа. На странице отображается сохранённая из попапа

--- script.js
+++ script.js
@@ -5,20 +5,35 @@ let popupClose = document.querySelector('.popup__close-img');
 function open() {
     popup.classList.add('popup_open');
 }
+
 pen.addEventListener('click', open);
 
 
 function close() {
     popup.classList.remove('popup_open');
 }
+
 popupClose.addEventListener('click', close);
 
 
 
 
-let heart = document.querySelectorAll('.element__heart');
+let formElement = document.querySelector('.form');
+let nameInput = document.querySelector("input[name='name']");
+let jobInput = document.querySelector("input[name='description']");
+let profileTitle = document.querySelector('.profile__title');
+let profileText = document.querySelector('.profile__text');
+let closeAndSave = document.querySelector('.form__button');
 
-function heartClick() {
-    heart.classList.add('element__heart_active');
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileText.textContent = jobInput.value;
 }
-heart.addEventListener('click', heartClick);
+
+function closePopup() {
+    popup.classList.add('popup_close')
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+formElement.addEventListener('submit', closePopup);
