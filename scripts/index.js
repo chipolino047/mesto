6c985076686c042569a9a943f615472b4881d004
Vladimir Kurenkov <chipolino047@gmail.com>
2023-03-18 20:18:38 +0400

    Fix: Исправил после замечаний 2 иттерации

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,16 @@
-let pen = document.querySelector('.profile__container-pen');
+let pen = document.querySelector('.profile__button-pen');
 let popup = document.querySelector('.popup');
 let popupClose = document.querySelector('.popup__close-img');
 let formElement = document.querySelector('.form');
-let nameInput = document.querySelector('.form__text_name');
-let jobInput = document.querySelector('.form__text_description');
+let nameInput = document.querySelector('.form__text_line_title');
+let jobInput = document.querySelector('.form__text_line_description');
 let profileTitle = document.querySelector('.profile__title');
 let profileText = document.querySelector('.profile__text');
 
 function open() {
     popup.classList.add('popup_opened');
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileText.textContent;
 }
 
 function close() {
