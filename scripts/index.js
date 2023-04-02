5d1acdeb25f3c5f030a8307d91bfbf4f38fd589e
Vladimir Kurenkov <chipolino047@gmail.com>
2023-04-02 13:32:31 +0400

    feat: создал новую ветку, для создания темплейт элементов

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,32 @@ let nameInput = document.querySelector('.form__text_line_title');
 let jobInput = document.querySelector('.form__text_line_description');
 let profileTitle = document.querySelector('.profile__title');
 let profileText = document.querySelector('.profile__text');
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
 
 function open() {
     popup.classList.add('popup_opened');
